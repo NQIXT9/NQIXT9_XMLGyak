896317a1366da94946c460fda660551a38e6a5e9
szarka8 <91125703+szarka8@users.noreply.github.com>
2021-11-08 22:07:52 +0100

    Add files via upload

--- NQIXT9_1026/DomReadNQIXT9.java
+++ NQIXT9_1026/DomReadNQIXT9.java
@@ -1,56 +1,48 @@
 package domnqixt91026;
 
-public class User {
-    private int id;
-    private String firstname;
-    private String lastname;
-    private String profession;
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
-    public User(int id, String firstname, String lastname, String profession) {
-        this.id = id;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.profession = profession;
-    }
+public class DomReadNQIXT9 {
 
-    @Override
-    public String toString() {
-        return "firstname=" + firstname + ", profession=" + profession + ", lastname=" + lastname;
-    }
-
-    public User() {
-    }
-
-    public String getfirstname() {
-        return firstname;
-    }
-
-    public void setfirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getlastname() {
-        return lastname;
-    }
-
-    public void setlastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getprofession() {
-        return profession;
-    }
-
-    public void setprofession(String profession) {
-        this.profession = profession;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+	public static void main(String[] args) {
+		try {
+			File sourceFile = new File("users1NQIXT9.xml");
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = factory.newDocumentBuilder();
+			Document doc = dBuilder.parse(sourceFile);
+			doc.getDocumentElement().normalize();
+			System.out.println("Root element: "+ doc.getDocumentElement().getNodeName());
+			ReadCurrentElement(doc);
+		}catch(ParserConfigurationException pce) {
+			pce.printStackTrace();
+		}catch(IOException ioe) {
+			ioe.printStackTrace();
+		}catch(SAXException sae) {
+			sae.printStackTrace();
+		}
+	}
+	
+	public static void ReadCurrentElement(Document doc) {
+		NodeList nList = doc.getElementsByTagName("user");
+		for (int i = 0; i < nList.getLength(); i++) {
+			Node nNode = nList.item(i);
+			Element element = (Element) nNode;
+			
+			if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+					String id = element.getAttribute("id");
+					String firstname = element.getElementsByTagName("firstname").item(0).getTextContent();
+					String lastname = element.getElementsByTagName("lastname").item(0).getTextContent();
+					String profession = element.getElementsByTagName("profession").item(0).getTextContent();
+					
+					System.out.println("\nCurrent element: "+ nNode.getNodeName());
+					System.out.println("User id "+ id + "\nFirst name:"+firstname+"\nLast name:"+lastname+"\nProfession:"+profession);
+				
+			}
+		}
+	}
 
 }
